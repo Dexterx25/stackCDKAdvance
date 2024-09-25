a65c9f217f1e7beec3e361f5338120ccedb28baf
Dexterx25 <davidblancokdf@gmail.com>
2024-09-25 18:57:43 -0500

    feat: in progress istio mesh

--- lib/scripts/kialycomplements/index.bash
+++ lib/scripts/kialycomplements/index.bash
@@ -4,3 +4,12 @@ kubectl apply -f https://raw.githubusercontent.com/istio/istio/release-1.13/samp
 kubectl patch svc grafana -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl patch svc prometheus -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
 kubectl patch svc kiali -n istio-system -p '{"spec": {"type": "LoadBalancer"}}'
+
+
+##watch istio versio:
+
+kubectl get pod -n istio-system -l app=istiod -o jsonpath="{.items[0].spec.containers[0].image}"
+
+
+kubectl get crd gateways.gateway.networking.k8s.io &> /dev/null || \
+  { kubectl kustomize "github.com/kubernetes-sigs/gateway-api/config/crd?ref=v1.1.0" | kubectl apply -f -; }
